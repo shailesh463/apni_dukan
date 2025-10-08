08dc31c5593ebcba7337cc5d8cab7aca3741f9d8
jaijai2344658 <shaileshagrawal464@gmail.com>
2025-10-08 16:19:12 +0530

    djhd

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+  const firebaseConfig = {
+        apiKey: "AIzaSyD0-sLNQ2st6UqRfmbDDUNpxVvGo6pGnsM",
+        authDomain: "apni-dukan-8808b.firebaseapp.com",
+        projectId: "apni-dukan-8808b",
+        storageBucket: "apni-dukan-8808b.firebasestorage.app",
+        messagingSenderId: "241019689449",
+        appId: "1:241019689449:web:3e919cd70f404bd1629378"
+    };
+    const ADMIN_WHATSAPP_NUMBER="8534906990";
